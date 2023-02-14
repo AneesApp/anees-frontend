a9acf69c625391dbad30085c3b456be5864431e9
Khalid Waleed <khalidwaleed0@outlook.com>
2023-02-14 20:48:15 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Registration from "./pages/Authentication/RegistrationPage";
+import Login from "./pages/Authentication/LoginPage";
 
 function App() {
-	return <HomePage />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/register" element={<Registration />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
